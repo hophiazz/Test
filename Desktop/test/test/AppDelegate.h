4ef6a0b805978339ce2de529b21b3d83acf15ae6
赵玉凤 <2289302248@qq.com>
2016-09-04 20:18:26 +0800

    test

---
+++ Desktop/test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 赵玉凤 on 16/9/4.
+//  Copyright © 2016年 ZYF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
